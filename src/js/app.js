2aad5b16c1a8e4691a8282229185b686a46aab56
vadymsem <vadym.sem18@gmail.com>
2017-06-30 12:01:40 +0300

    added full structure and fixed some bugs with link routes. It works good for now

--- src/js/app.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello HTML5 Boilplate!');
-console.info('Constructed by Vadym Semenets   ___   facebook.com/vadym.semenets');
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
 
 
 //= app.js
